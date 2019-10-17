e29141c3d7afd0c90e53a9cfceb83bd0966a24d3
Mackenzie Clark <mackenzie.a.z.c@gmail.com>
2019-10-16 21:41:19 -0700

    first

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use actix_web::{web, App, HttpServer, Responder, HttpResponse};
+
+fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+fn index2() -> impl Responder {
+    HttpResponse::Ok().body("Hello world again!")
+}
+
+fn main() {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(index))
+            .route("/healthz", web::get().to(index2))
+    })
+        .bind("127.0.0.1:8088")
+        .unwrap()
+        .run()
+        .unwrap();
+}
